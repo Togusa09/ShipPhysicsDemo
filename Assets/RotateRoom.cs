dbc1ce296704aba8f5d3d2c1433f3ae666b1a13e
Ben Hockley <ben.hockley@readify.net>
2020-02-05 10:25:54 +1000

    Initial commit

---
+++ Assets/RotateRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var angle = Time.fixedTime / (360 * 100);
+        transform.RotateAround(transform.position, transform.right, 2 * Time.deltaTime);
+    }
+}
